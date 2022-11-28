d5a1ae3d909c2aad86f157a3ca49d13ff4f2216b
Jason Yang <jasonxuyang@gmail.com>
2022-11-27 22:21:18 -0500

    feature: add events

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,56 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { useTimer } from "use-timer";
 import styles from "../styles/Home.module.css";
-import { INITAL_STATE, IState, TIMER_STATUS } from "../types";
+import {
+  EVENTS,
+  EVENT_PROMPT_TIME,
+  IEvent,
+  IState,
+  TIMER_STATUS,
+} from "../types";
 
 export default function Game(pageProps: { initialState: IState }) {
+  //
+  // // Hooks
+  //
   const [state, setState] = useState<IState>(pageProps.initialState);
+  const { level, levelTime, timePerLevel, event, completedEvents } = state;
   const { time, start, pause, reset, status, advanceTime } = useTimer({
     initialTime: 0,
-    endTime: state.timePerLevel,
+    endTime: timePerLevel,
     onTimeUpdate: () => {
-      setState({
-        ...state,
-        levelTime: time,
-        isPaused: status === TIMER_STATUS.PAUSED,
-      });
+      if (time >= EVENT_PROMPT_TIME) {
+        const event =
+          EVENTS.find((event) => {
+            return event.level === level;
+          }) || null;
+
+        if (!completedEvents.some((event) => event?.level === level)) {
+          setState({
+            ...state,
+            event: event,
+            levelTime: time,
+            isPaused: status === TIMER_STATUS.PAUSED,
+          });
+          pause();
+        } else {
+          setState({
+            ...state,
+            levelTime: time,
+          });
+        }
+      } else {
+        setState({
+          ...state,
+          levelTime: time,
+        });
+      }
       sessionStorage.setItem("state", JSON.stringify(state));
     },
     onTimeOver: () => {
       setState({
         ...state,
-        level: state.level + 1,
+        level: level + 1,
         isPaused: status === TIMER_STATUS.PAUSED,
       });
       reset();
@@ -35,13 +66,39 @@ export default function Game(pageProps: { initialState: IState }) {
     start();
   }, [advanceTime, pageProps.initialState.levelTime, start]);
 
+  const consumeEvent = () => {
+    const updatedCompletedEvents = [...completedEvents, event as IEvent];
+    setState({
+      ...state,
+      completedEvents: updatedCompletedEvents,
+      event: null,
+      levelTime: time,
+    });
+    start();
+  };
+
+  //
+  // // Render
+  //
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div>Current Time: {state.levelTime}</div>
-        <div>Current Level: {state.level}</div>
+        <div>Current Time: {levelTime}</div>
+        <div>Current Level: {level}</div>
         <button onClick={pause}>Pause Timer</button>
         <button onClick={start}>Start Timer</button>
+        <div>Current Event: {event ? event.prompt : "No event active"}</div>
+        {event ? (
+          <>
+            {event.choices.map((choice, index) => {
+              return (
+                <button key={index} onClick={consumeEvent}>
+                  {choice.text}
+                </button>
+              );
+            })}
+          </>
+        ) : null}
       </main>
     </div>
   );
--- types.ts
+++ types.ts
@@ -1,16 +1,56 @@
 export const SECOND = 1;
 export const MINUTE = 60 * SECOND;
-export const INITIAL_TIME_PER_LEVEL = 5;
+export const INITIAL_TIME_PER_LEVEL = 5 * MINUTE;
+export const EVENT_PROMPT_TIME = 5;
 export const TOTAL_LEVELS = 6;
 
-export const INITAL_STATE = {
-  level: 0,
+export const INITAL_STATE: IState = {
+  level: 1,
   levelTime: 0,
   timePerLevel: INITIAL_TIME_PER_LEVEL,
   studiousLevel: 0,
   socialLevel: 0,
   isPaused: false,
+  event: null,
+  completedEvents: [],
 };
+
+export const EVENTS: IEvent[] = [
+  {
+    level: 1,
+    levelTime: EVENT_PROMPT_TIME,
+    prompt: "Test Event 1",
+    choices: [
+      {
+        text: "Choice 1",
+        attribute: "social",
+        effect: "hint",
+      },
+      {
+        text: "Choice 2",
+        attribute: "social",
+        effect: "hint",
+      },
+    ],
+  },
+  {
+    level: 2,
+    levelTime: EVENT_PROMPT_TIME,
+    prompt: "Test Event 2",
+    choices: [
+      {
+        text: "Choice 1",
+        attribute: "social",
+        effect: "hint",
+      },
+      {
+        text: "Choice 2",
+        attribute: "social",
+        effect: "hint",
+      },
+    ],
+  },
+];
 
 export enum TIMER_STATUS {
   RUNNING = "RUNNING",
@@ -25,8 +65,19 @@ export interface IState {
   studiousLevel: number;
   socialLevel: number;
   isPaused: boolean;
+  event: IEvent | null;
+  completedEvents: IEvent[];
 }
 
 export interface IEvent {
-  gameTime: number;
+  level: number;
+  levelTime: number;
+  prompt: string;
+  choices: IEventChoice[];
+}
+
+export interface IEventChoice {
+  text: string;
+  attribute: "studious" | "social";
+  effect: "timePerLevel" | "hint";
 }
